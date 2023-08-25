4fb55b8a17268a62c406c642f7227535033623a2
Kate Smith <kate.smith@digital.justice.gov.uk>
2023-08-25 12:01:06 +0100

    solves simple calculations with input type functions

--- boolean_calculator.go
+++ boolean_calculator.go
@@ -1,9 +1,47 @@
 package go_boolean_calculator
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 type Calculator struct {
-	Input string
+	Input      []string
+	BoolInputs []bool
 }
 
 func (c Calculator) Run() bool {
-	return true
+
+	fmt.Println("Input within the run function")
+	fmt.Println(c.Input)
+	fmt.Println("length")
+	fmt.Println(len(c.Input))
+	value, _ := strconv.ParseBool(c.Input[0])
+	return value
+}
+
+func CreateCalculator(input string) Calculator {
+	inputAsArray := strings.Split(input, ",")
+	cal := Calculator{Input: inputAsArray}
+	cal.ConvertSingleValues(cal.Input)
+	return cal
+}
+
+func (c Calculator) ConvertSingleValues(input []string) {
+	value := false
+
+	for i := 0; i < len(input); i++ {
+		fmt.Println(i, input[i])
+		if strings.Contains(input[i], "NOT") {
+			value = !strings.Contains(input[i], "TRUE")
+		} else if strings.Contains(input[i], "AND") {
+			value = !strings.Contains(input[i], "FALSE")
+		} else if strings.Contains(input[i], "OR") {
+			value = strings.Contains(input[i], "TRUE")
+		} else {
+			value = strings.Contains(input[i], "TRUE")
+		}
+		c.BoolInputs = append(c.BoolInputs, value)
+	}
 }
--- boolean_calculator_test.go
+++ boolean_calculator_test.go
@@ -6,24 +6,50 @@ import (
 	"testing"
 )
 
-func Test_UpdateItems(t *testing.T) {
+//func Test_Simple_Inputs(t *testing.T) {
+//	tests := []struct {
+//		Input          string
+//		ExpectedOutput bool
+//	}{
+//		{Input: "TRUE", ExpectedOutput: true},
+//		{Input: "FALSE", ExpectedOutput: false},
+//		{Input: "NOT TRUE", ExpectedOutput: false},
+//		{Input: "NOT FALSE", ExpectedOutput: true},
+//		{Input: "TRUE AND TRUE", ExpectedOutput: true},
+//		{Input: "TRUE AND FALSE", ExpectedOutput: false},
+//		{Input: "FALSE AND FALSE", ExpectedOutput: false},
+//		{Input: "TRUE OR TRUE", ExpectedOutput: true},
+//		{Input: "TRUE OR FALSE", ExpectedOutput: true},
+//		{Input: "FALSE OR FALSE", ExpectedOutput: false},
+//	}
+//	for k, tc := range tests {
+//		t.Run("scenario "+strconv.Itoa(k+1)+" given:"+tc.Input, func(t *testing.T) {
+//			calculator := CreateCalculator(tc.Input)
+//			assert.Equal(t, calculator.Run(), tc.ExpectedOutput)
+//		})
+//	}
+//}
+
+func Test_Longer_Inputs(t *testing.T) {
 	tests := []struct {
 		Input          string
 		ExpectedOutput bool
 	}{
-		{
-			Input:          "TRUE",
-			ExpectedOutput: true,
-		},
-		{
-			Input:          "FALSE",
-			ExpectedOutput: false,
-		},
+		{Input: "TRUE OR TRUE OR TRUE AND FALSE", ExpectedOutput: true},
+		{Input: "TRUE OR FALSE AND NOT FALSE", ExpectedOutput: true},
+		//{Input: "NOT TRUE", ExpectedOutput: false},
+		//{Input: "NOT FALSE", ExpectedOutput: true},
+		//{Input: "TRUE AND TRUE", ExpectedOutput: true},
+		//{Input: "TRUE AND FALSE", ExpectedOutput: false},
+		//{Input: "FALSE AND FALSE", ExpectedOutput: false},
+		//{Input: "TRUE OR TRUE", ExpectedOutput: true},
+		//{Input: "TRUE OR FALSE", ExpectedOutput: true},
+		//{Input: "FALSE OR FALSE", ExpectedOutput: false},
 	}
 	for k, tc := range tests {
-		t.Run("scenario "+strconv.Itoa(k+1), func(t *testing.T) {
-			actualOutput := Calculator{tc.Input}.Run()
-			assert.Equal(t, actualOutput, tc.ExpectedOutput)
+		t.Run("scenario "+strconv.Itoa(k+1)+" given:"+tc.Input, func(t *testing.T) {
+			calculator := CreateCalculator(tc.Input)
+			assert.Equal(t, calculator.Run(), tc.ExpectedOutput)
 		})
 	}
 }
